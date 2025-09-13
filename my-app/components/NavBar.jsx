14588bdfc0ffbec92785efb683406d9cdab2a721
Aboubakary Traore <Aboubakarytr1@gmail.com>
2025-09-13 16:31:40 -0400

    Update NavBar.jsx

--- my-app/components/NavBar.jsx
+++ my-app/components/NavBar.jsx
@@ -1,22 +1,8 @@
 export default function() {
   return (
-    <div className="w-full bg-red-500 flex justify-end items-center p-4 space-x-6 shadow-md rounded-xl">
-      <a 
-        href="#" 
-        className="text-white text-lg font-semibold hover:text-yellow-200 transition-colors duration-200"
-      >
-        Get your BMI
-      </a>
-      <a 
-        href="#" 
-        className="text-white text-lg font-semibold hover:text-yellow-200 transition-colors duration-200"
-      >
-        Import your medical history
-      </a>
-
-
-      
+    <div className="w-full bg-red-500 flex justify-center items-center space-x-6 shadow-md p-5">
+      <a href="#" className="text-white text-lg font-semibold hover:text-yellow-200 transition-colors duration-200">Get your BMP</a>
+      <a href="#" className="text-white text-lg font-semibold hover:text-yellow-200 transition-colors duration-200">Import your medical history</a>
     </div>
-
-  );
+    );
 }
